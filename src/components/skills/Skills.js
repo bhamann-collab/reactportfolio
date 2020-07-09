9ae89336a6c57791a1c875ed6598243c86e32745
bhamann-collab <brockohamann@gmail.com>
2020-07-09 21:34:40 +0930

    Skills images code is now optimized

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import images from '../../img/skillsImg'
+
+
+
 
 const Skills = () => {
     return (
@@ -6,65 +10,12 @@ const Skills = () => {
             <h1>Skills</h1>
             <div className="headingLine"></div>
             <div className="container">
-                    <div className="skillComponent">
-                        <h3>React</h3>
-                        <img src="./img/font-awesome/react.svg" alt="Node" className="extraPadding" />
-                    </div>
-                    <div className="skillComponent">
-                        <h3>GitHub</h3>
-                        <img src="./img/font-awesome/github-square.svg" alt="Git" />
-                    </div>
-                    
-                    <div className="skillComponent">
-                        <h3>HTML</h3>
-                        <img src="./img/font-awesome/html5_new.svg" alt="HTML" />
-                    </div>
-                    
-                    <div className="skillComponent">
-                        <h3>CSS</h3>
-                        <img src="./img/font-awesome/css3_new.svg" alt="CSS" />
-                    </div>
-                    
-                    <div className="skillComponent">
-                        <h3>SASS</h3>
-                        <img src="./img/font-awesome/sass_new.svg" alt="SASS" className="extraPadding" />
-                    </div>
-                    
-                    <div className="skillComponent">
-                        <h3>Javascript</h3>
-                        <img src="./img/font-awesome/js_new.svg" alt="Javascript" className="extraPadding" />
-                    </div>
-                    
-                    <div className="skillComponent">
-                        <h3>Node.js</h3>
-                        <img src="./img/font-awesome/node_new.svg" alt="Node" className="extraPadding" />
-                    </div>
-
-                    <div className="skillComponent">
-                        <h3>MySQL</h3>
-                        <img src="./img/font-awesome/mysql.svg" alt="MySql" className="extraPadding" />
-                    </div>
-
-                    <div className="skillComponent">
-                        <h3>Sequelize</h3>
-                        <img src="./img/font-awesome/sequelize.svg" alt="Sequelize" className="extraPadding" />
-                    </div>
-
-                    <div className="skillComponent">
-                        <h3>Handlebars</h3>
-                        <img src="./img/font-awesome/handlebarsjs-icon.svg" alt="Handlebars" className="extraPadding" />
-                    </div>
-
-                    <div className="skillComponent">
-                        <h3>MongoDB</h3>
-                        <img src="./img/font-awesome/mongodb-icon.svg" alt="MongoDB" className="extraPadding" />
-                    </div>
-
-                    <div className="skillComponent">
-                        <h3>Bootstrap</h3>
-                        <img src="./img/font-awesome/bootstrap-4.svg" alt="Bootstrap" className="extraPadding" />
-                    </div>
-                </div>
+                { images.map(({id, src, name}) => 
+                <div className="skillComponent">
+                    <h3>{name}</h3>
+                    <img key={id} src={src} className="extraPadding"/>
+                </div>)}
+            </div>
         </section>
 
     )
---
+++ src/img/skillsImg.js
@@ -0,0 +1,16 @@
+const images = [
+    {id: 1, name: 'React', src: '/static/media/react.4009eef3.svg'},
+    {id: 2, name: 'GitHub', src: '/static/media/github-square.358847e2.svg'},
+    {id: 3, name: 'HTML', src: '/static/media/html5_new.81faebdb.svg'},
+    {id: 4, name: 'CSS', src: '/static/media/css3_new.62d97e17.svg'},
+    {id: 5, name: 'SASS', src: '/static/media/sass_new.b446ca5a.svg'},
+    {id: 6, name: 'Javascript', src: '/static/media/js_new.61ed8b05.svg'},
+    {id: 7, name: 'Node.js', src: '/static/media/node_new.5c376133.svg'},
+    {id: 8, name: 'MySQL', src: '/static/media/mysql.f80a91ba.svg'},
+    {id: 9, name: 'Sequelize', src: '/static/media/sequelize.43c9d125.svg'},
+    {id: 10, name: 'Handlebars', src: '/static/media/handlebarsjs-icon.bd464a4a.svg'},
+    {id: 11, name: 'MongoDB', src: '/static/media/mongodb-icon.554c7d4b.svg'},
+    {id: 12, name: 'Bootstrap', src: '/static/media/bootstrap-4.ffded238.svg'}
+]
+
+export default images;
