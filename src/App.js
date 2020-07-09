84d0a898fd323e26735c2040a0cdf7166de41613
bhamann-collab <brockohamann@gmail.com>
2020-07-09 17:29:42 +0930

    Put background image behind text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/style.scss';
 import TitlePage from './components/titlePage/TitlePage'
+import AboutMe from './components/aboutMe/AboutMe' 
 
 function App() {
   return (
     <div className="App">
       <TitlePage />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../css/_global.scss';
+
+const aboutMe = () => {
+    return (
+
+        <section id="navAboutMe">
+            <div className="container">
+                <h2><b>Hey there,</b></h2>
+                <br />
+                <h2>I am an Adelaide-based web developer with an engineering background</h2>
+                <br />
+                <h2>I enjoy creating and applying ideas through technology to improve the lives of others.</h2> 
+                <br />
+                <h2>Feel free to look at my past work and get in touch below.</h2>
+            </div>
+        </section>
+    )
+}
+
+export default aboutMe
